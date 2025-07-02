2396392542a18639923c0f18742ba8dcc367ad60
Jayanth710 <vunnamjayanth7@gmail.com>
2025-07-02 01:42:39 -0600

    backend updates

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,20 +1,21 @@
 import { Router } from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-import { upload, handleFileUpload, getUploads } from '../handlers/uploadHandler.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { upload, handleFileUpload } from '../handlers/uploadHandler.js';
 import { verifyToken } from '../middleware/auth.js';
+// #getUploads
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const uploadRouter = Router();
 
 // Serve upload.html from the same folder (routes/)
-// uploadRouter.get('/', (_req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/upload.html'));
-// });
+uploadRouter.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/upload.html'));
+});
 
 // Handle file upload
 uploadRouter.post('/', verifyToken, upload.single('file'), handleFileUpload);
-uploadRouter.get('/', verifyToken, getUploads)
+// uploadRouter.get('/', verifyToken, getUploads)
 
 export default uploadRouter;
