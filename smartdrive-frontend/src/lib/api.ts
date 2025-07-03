8246285c8c9d07d9031c11dd6f88e42800ef1dfe
Jayanth710 <vunnamjayanth7@gmail.com>
2025-07-03 09:10:49 -0600

    Added backend url for vercel

--- smartdrive-frontend/src/lib/api.ts
+++ smartdrive-frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:4000',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000',
 });
 
 // --- Interceptor ---
